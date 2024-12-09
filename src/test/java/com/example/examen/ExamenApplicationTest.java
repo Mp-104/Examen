781a2392a78a1a4f59d551a67d74532cb6a47ed0
mp-104 <mp-104@hotmail.com>
2024-12-09 16:01:07 +0100

    first commit

---
+++ src/test/java/com/example/examen/ExamenApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExamenApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
