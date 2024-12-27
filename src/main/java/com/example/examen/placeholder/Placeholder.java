262a7498d219440671bf6a3d33626378abe93aa3
Maksim <Mp-104@hotmail.com>
2024-12-27 13:18:49 +0100

    Error handle placeholderImage + minor design tweaks for register-page

--- src/main/java/com/example/examen/placeholder/Placeholder.java
+++ src/main/java/com/example/examen/placeholder/Placeholder.java
@@ -3,6 +3,7 @@ package com.example.examen.placeholder;
 import org.apache.commons.io.IOUtils;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Placeholder {
@@ -11,11 +12,14 @@ public class Placeholder {
 
         try {
 
-            FileInputStream fileInputStream = new FileInputStream("src/main/resources/placeholder");
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/placeholder/sdf");
             String placeholderImage = IOUtils.toString(fileInputStream, "UTF-8");
 
             //System.out.println("placeholderImage: " + placeholderImage.toString());
             return placeholderImage;
+
+        } catch (FileNotFoundException e) {
+            return null;
 
         } catch (IOException e) {
             throw new RuntimeException(e);
